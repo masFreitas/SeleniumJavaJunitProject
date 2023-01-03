04268035995674c25d661ecf6b3cf7d0222effa4
Mateus GitHub <mateusasfreitas@gmail.com>
2023-01-02 21:38:47 -0300

    Create random email generator with java Random

--- src/test/java/support/Utils.java
+++ src/test/java/support/Utils.java
@@ -5,10 +5,25 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import runner.RunnerTest;
 
+import java.util.Random;
+
 public class Utils extends RunnerTest {
 
     public void waitElementToBePresentOnPage(By element, int time) {
         WebDriverWait wait = new WebDriverWait(driver, time);
         wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public String getRandomEmail() {
+        String emailName = "mateus_";
+        String emailProvider = "freitas.com";
+
+        Random random = new Random();
+        int min = 1;
+        int max = 999999999;
+        int result = random.nextInt(max-min) + min;
+
+        return emailName + result + emailProvider;
+
     }
 }
